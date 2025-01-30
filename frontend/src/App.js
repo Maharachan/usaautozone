28000f5fed97f25e2d80cfdbec071861e17efd4b
Krishna Tummalapalli <tnlkrishna4321@gmail.com>
2025-01-31 00:13:07 +0530

    30-1-25

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { scroller } from "react-scroll"; // Import scroller for smooth scrolling
+import { scroller } from "react-scroll";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Navbar from "./components/Navbar";
@@ -10,28 +10,24 @@ import Section1 from "./components/Section1";
 import Section3 from "./components/Section3";
 import Section5 from "./components/Section5";
 import Cars from "./components/cars";
-import CarDetails from "./components/cardetails";
+import CarDetailsPage from "./components/cardetails"; // Import updated component
 import AdminPage from "./components/Admin";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
 import "./App.css";
 
-// ScrollToSection component handles the scroll after navigation
 function ScrollToSection() {
   const location = useLocation();
-
   useEffect(() => {
-    // Check if location.state.scrollTo is passed for scrolling
     if (location.state?.scrollTo) {
       scroller.scrollTo(location.state.scrollTo, {
         smooth: true,
         duration: 500,
-        offset: -70, // Adjust if you have a fixed navbar
+        offset: -70,
       });
     }
   }, [location]);
-
-  return null; // This component does not render anything
+  return null;
 }
 
 function App() {
@@ -43,11 +39,10 @@ function App() {
 
   return (
     <Router>
-      <ScrollToSection /> {/* Component to handle scrolling after navigation */}
+      <ScrollToSection />
       <div className="App">
         <Navbar />
         <Routes>
-          {/* Public routes */}
           <Route
             path="/"
             element={
@@ -57,19 +52,11 @@ function App() {
                 <Section1 />
                 <Section3 />
                 <Section5 />
-                
               </>
             }
           />
           <Route path="/Cars" element={<Cars />} />
-          <Route path="/Cars/:id" 
-          element={
-            <>
-            <CarDetails />
-            </>
-          } /> {/* Dynamic route for car details */}
-
-          {/* Protected route for AdminPage */}
+          <Route path="/Cars/:id" element={<CarDetailsPage />} />
           <Route
             path="/admin"
             element={
