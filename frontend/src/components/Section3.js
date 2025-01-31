3e9ac2cef6c72f0ad0ad8231637b0467e2a96059
Krishna Tummalapalli <tnlkrishna4321@gmail.com>
2025-02-01 01:02:00 +0530

    31-1-2025

--- frontend/src/components/Section3.js
+++ frontend/src/components/Section3.js
@@ -1,29 +1,49 @@
 import React from "react";
-import { FaPhoneAlt, FaEnvelope } from "react-icons/fa"; // Import icons
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import "./Section3.css";
 
 const Section3 = () => {
+  const phoneNumber = "9039780137";
+  const emailAddress = "sasif4ever@yahoo.com";
+
   return (
     <div className="section3">
-      {/* Centered Contact Card */}
       <div className="section3-card">
         <h2 className="section3-title">Contact Us</h2>
         <div className="section0-divider1"></div>
+
+        {/* Contact Information Section */}
         <div className="contact-info">
-          {/* Phone Section */}
+          {/* Call Section */}
           <div className="contact-item">
-            <FaPhoneAlt className="icon large-icon" />
-            <div>
-              <span className="contact-label">Call Us</span>
-              <span className="contact-text">+1 903 978 0137</span>
+            <FaPhoneAlt className="large-icon" />
+            <div className="contact-details-wrapper">
+              <div className="contact-details">
+                <span className="contact-label">CALL US : {phoneNumber}</span>
+              </div>
+              <button
+                className="contact-button1"
+                onClick={() => window.location.href = `tel:${phoneNumber}`}
+              >
+                Call Now
+              </button>
             </div>
           </div>
-          {/* Mail Section */}
+
+          {/* Email Section */}
           <div className="contact-item">
-            <FaEnvelope className="icon large-icon" />
-            <div>
-              <span className="contact-label">Email Us</span>
-              <span className="contact-text">info@carsales.com</span>
+            <FaEnvelope className="large-icon" />
+            <div className="contact-details-wrapper">
+              <div className="contact-details">
+                <span className="contact-label">EMAIL US : {emailAddress}</span>
+                
+              </div>
+              <button
+                className="contact-button1"
+                onClick={() => window.location.href = `mailto:${emailAddress}`}
+              >
+                Email Now
+              </button>
             </div>
           </div>
         </div>
